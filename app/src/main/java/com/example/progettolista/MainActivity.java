48252204b139faf42e70674388cc997a4271b124
FrancescoBasso <frnacescobasso03@gmail.com>
2021-12-03 14:48:16 +0100

    Lista Creata

--- app/src/main/java/com/example/progettolista/MainActivity.java
+++ app/src/main/java/com/example/progettolista/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.progettolista;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-
+ ListView Listas;
+ String aStati[] = {"Italia","Francia","Germania","Svizzera"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Listas=(ListView)findViewById(R.id.Listastati);
+        ArrayAdapter<String> aa =new ArrayAdapter<String>(this,R.layout.activity_main,R.id.Listastati,aStati);
+        Listas.setAdapter(aa);
     }
+
+
 }
