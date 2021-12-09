acb91c1a72a584323bb49d37adda8c66fb9abfec
FrancescoBasso <frnacescobasso03@gmail.com>
2021-12-09 08:51:57 +0100

    SetOnClickListener

--- app/src/main/java/com/example/progettolista/MainActivity.java
+++ app/src/main/java/com/example/progettolista/MainActivity.java
@@ -3,19 +3,30 @@ package com.example.progettolista;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
- ListView Listas;
+ ListView listas;
  String aStati[] = {"Italia","Francia","Germania","Svizzera"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Listas=(ListView)findViewById(R.id.Listastati);
+        final Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(this,"Benvenuti", Toast.LENGTH_SHORT).show();
+            }
+        });
+        listas=(ListView)findViewById(R.id.Listastati);
         ArrayAdapter<String> aa =new ArrayAdapter<String>(this,R.layout.activity_main,R.id.Listastati,aStati);
-        Listas.setAdapter(aa);
+        listas.setAdapter(aa);
     }
 
 
